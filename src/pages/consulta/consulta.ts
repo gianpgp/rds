c6c6b93d737fab47e7e6002e0672207ce05b558e
gianpgp <gianpgp@primeinformatica.com.br>
2018-09-17 15:09:10 -0300

    app com consulta

--- src/pages/consulta/consulta.ts
+++ src/pages/consulta/consulta.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import Parse from 'parse';
 
 /**
  * Generated class for the ConsultaPage page.
@@ -14,12 +15,55 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'consulta.html',
 })
 export class ConsultaPage {
+  conteudo = [];
+  ag = Parse.Object.extend("Agendamentos");
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  sucesso(results, conteudo) {
+    for (var i in results) {
+
+      conteudo.push(
+        {
+          Sala: results[i].get("Sala"),
+          Inicio: results[i].get("Inicio"),
+          Fim: results[i].get("Fim"),
+          Desc: results[i].get("Descricao"),
+          // User:results[i].user.get("Nome")
+
+        }
+      )
+
+
+
+    }}
+
+    
+    getSalas(){
+     
+      var cat = this;
+      var query = new Parse.Query(this.ag);
+      // query.include("User");
+      //query.equalTo("Disponivel",true);
+      query.find({
+
+        success: function (results) {
+          return cat.sucesso(results, cat.conteudo);
+
+        }, error: function (error) {
+          console.log(error.mesage);
+        }
+
+
+      });
+    }
+
+    constructor(public navCtrl: NavController, public navParams: NavParams) {
+    }
+
+    ionViewDidLoad() {
+      console.log('ionViewDidLoad ConsultaPage');
+      this.getSalas();
+      console.log(this.conteudo)
+
+    }
+
   }
-
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad ConsultaPage');
-  }
-
-}
